cd9a086ab8cadfc4bd7afc4b69980ca56820474f
chiehyang <chiehyang@tencent.com>
2016-07-08 19:34:43 +0800

    Reload from example. Signed-off-by: chiehyang <chiehyang@tencent.com>

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hello, npm!');
+import 'babel-polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
+
+let store = createStore(todoApp)
+
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
